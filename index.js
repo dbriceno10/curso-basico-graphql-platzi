c7ed40305caee655ec304149e4dd333bbac7ed45
Daniel Briceño <daniel.b100726@gmail.com>
2022-03-18 18:21:04 -0400

    Clase 4: Queries y Resolvers, comentarios

--- index.js
+++ index.js
@@ -27,3 +27,28 @@ const resolvers = {
 graphql(schema, "{ hello }", resolvers).then((data) => {
   console.log(data);
 });
+
+
+/*
+Los parámetros de graphql se pasan como un objeto
+Los resolvers se colocan del siguiente modo
+
+graphql({
+    schema: schema,
+    source: '{ saludo}',
+    rootValue: resolvers
+})
+.then((data) => {
+    console.log(data);
+})
+.catch(e => {
+    console.log(e);
+});
+
+
+Es posible llamar a más de un resolver:
+
+graphql(schema, '{ hello, saludo }', resolvers).then((data) => {
+    console.log(data)
+})
+ */
