8eee8d1af12e1612830f618bb5dfb76bcb85d993
Daniel Briceño <daniel.b100726@gmail.com>
2022-03-18 18:08:29 -0400

    Clase 4: Queries y Resolvers

--- index.js
+++ index.js
@@ -7,12 +7,23 @@ const { graphql, buildSchema } = require("graphql");
 const schema = buildSchema(`
 type Query {
   hello: String
+  saludo: String
 }
 `);
+
+//Configurar objetos resolvers
+
+const resolvers = {
+  //van a tener propiedades que deben tener propiedades igual al nombre de la query a ejecutar, cada vez que agrega una nueva query se debe definir un nuevo resolver
+  hello: () => {
+    return "Hello World";
+  },
+  saludo: () => "Hola Mundo",
+};
 
 //los tipos escalares en graphql son string, integer, float o boolean
 
 //ejecutar el query hello
-graphql(schema, "{ hello }").then((data) => {
+graphql(schema, "{ hello }", resolvers).then((data) => {
   console.log(data);
 });
